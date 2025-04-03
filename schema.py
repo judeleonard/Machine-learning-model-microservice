4b50d7648a1f8479b93091334cb513e03cafaf84
judeleonard <judeleonard86@gmail.com>
2025-04-03 11:29:20 +0100

    separated schema from main process

---
+++ schema.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class CustomerModel(BaseModel):
+    age: int
+    job: str
+    marital: str
+    education: str
+    default: str
+    balance: int
+    housing: str
+    loan: str
+    day: int
+    duration: int
+    campaign: int
+    pdays: int
+    previous: int
+    poutcome: str
